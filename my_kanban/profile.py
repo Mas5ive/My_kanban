6af017ae22a39af06cf4069cdedd2992be99c388
Mas5ive <empty>
2024-04-01 16:18:51 +0300

    refactor: change the way boards are queried
    instead of 2 queries to the database, 1 query is used.

--- my_kanban/profile.py
+++ my_kanban/profile.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from flask import Blueprint, render_template
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from sqlalchemy import select
@@ -14,17 +16,18 @@ bp = Blueprint('profile', __name__)
 def show():
     username = get_jwt_identity()
 
-    owner_boards = sqla.session.execute(
-        select(Board).
+    boards_info = sqla.session.execute(
+        select(Board, user_board.c.is_owner).
         join(user_board, user_board.c.board_id == Board.id).
-        where(user_board.c.username == username, user_board.c.is_owner == 1)
-    ).scalars().all()
+        where(user_board.c.username == username)
+    ).all()
 
-    invitation_boards = sqla.session.execute(
-        select(Board).
-        join(user_board, user_board.c.board_id == Board.id).
-        where(user_board.c.username == username, user_board.c.is_owner == 0)
-    ).scalars().all()
+    boards = defaultdict(list)
+    for board, is_owner in boards_info:
+        if is_owner:
+            boards['owner boards'].append(board)
+        else:
+            boards['invitation boards'].append(board)
 
     invitations = sqla.session.execute(
         select(Invitation, Board.title).
@@ -35,7 +38,7 @@ def show():
     return render_template(
         'profile.html',
         username=username,
-        owner_boards=owner_boards,
-        invitation_boards=invitation_boards,
+        owner_boards=boards['owner boards'],
+        invitation_boards=boards['invitation boards'],
         invitations=invitations
     )
